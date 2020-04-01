ef6ad7a6868e1d0483d27bf4c3edede0a4fafcfa
rafal <rafalsiniewicz@gmail.com>
2020-04-01 22:48:26 +0200

    removed read csv

--- src/read_from_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-def read_from_file(filename):
-	#headers = {'match_results': ('Date', 'Start', 'Visitor/Neutral', 'PTS_visitor', 'Home/Neutral', 'PTS_home', '', 'Overtime', 'Attend', 'Notes')}
-
-	df = pd.read_csv('../nba_stats/' + filename, index_col=None)
-	return df
-
-dataset = read_from_file('season_2016-17/match_results_may.csv')
-print(dataset.columns)
