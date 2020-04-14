bee86bdbde17cd0701c83c47f7ddbd51b70f8294
jagajda <jagajda@student.agh.edu.pl>
2020-04-14 21:55:35 +0200

    update script for getting NBA API data

--- src/get_data.py
+++ src/get_data.py
@@ -1,7 +1,23 @@
 from nba_api.stats.endpoints import leaguegamefinder
+from os import path, mkdir
+import pandas as pd
 
-gamefinder = leaguegamefinder.LeagueGameFinder(league_id_nullable='00')	# nba id is '00'
-games = gamefinder.get_data_frames()[0]
-print(games[['GAME_DATE']].head(5))
+PATH = ("../data/")
+FILENAME = "games.csv"
+
+def main():
+    gamefinder = leaguegamefinder.LeagueGameFinder(league_id_nullable='00')	# nba id is '00'
+    games = gamefinder.get_data_frames()[0]
+    if not os.path.exists(PATH):
+        os.mkdir(PATH)
+    if not os.path.exists(os.path.join(PATH, FILENAME)):
+        with open(os.path.join(PATH, FILENAME), 'w') as fp: 
+            pass
+    games.to_csv(os.path.join(PATH, FILENAME))
+
+
+if __name__ == "__main__"
+    main()
+
 
 
